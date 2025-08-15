6fd4a59ec5c891f98d42852b4756d30602cb6a74
Raed Salah Bzour <raedbzour77@gmail.com>
2025-08-15 14:58:53 +0300

    preparing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
 
-function App() {
+export default function App() {
+  let theme = createTheme({
+    palette: {
+      primary: {
+        main: "#29bebeff",
+      },
+      secondary: {
+        main: "#767b87ff",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container className="App"></Container>
+    </ThemeProvider>
   );
 }
-
-export default App;
